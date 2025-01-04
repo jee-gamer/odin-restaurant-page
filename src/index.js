76d1a8653cbc79b1004f285ba37d7ab6087bf72e
Jiramate Kedmake <jiramate2548@gmail.com>
2025-01-04 15:13:10 +0700

    make navbar skeleton

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./styles.css";
+
+const homeButton = document.createElement('button');
+homeButton.textContent = 'HOME';
+
+const menuButton = document.createElement('button');
+menuButton.textContent = 'MENU';
+
+const aboutButton = document.createElement('button');
+aboutButton.textContent = 'ABOUT';
+
+const navbar = document.querySelector('nav');
+navbar.appendChild(homeButton);
+navbar.appendChild(menuButton);
+navbar.appendChild(aboutButton);
+
+
+
